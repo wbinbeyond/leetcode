80d89f851d1a586a1ee0873a5aa64e338e3a4501
wbinbeyond <wbin.beyond@gmail.com>
2018-11-29 22:21:28 -0500

    Create Divide_Two_Integers.cpp

---
+++ Divide_Two_Integers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (divisor == 0) return INT32_MAX;
+        if (dividend == INT32_MIN) {
+            if (divisor == -1) return INT32_MAX;
+            else if (divisor == 1) return INT32_MIN;
+        }
+        long long divd = (long long) dividend;
+        long long divs = (long long) divisor;
+        int sign = 1;
+        if (divd < 0) {
+            divd = -divd;
+            sign = -sign;
+        }
+        if (divs < 0) {
+            divs = -divs;
+            sign = -sign;
+        }
+        int res = 0;
+        while (divd >= divs) {
+            int shift = 0;
+            while (divd >= (divs << shift)) {
+                shift++;
+            }
+            divd -= divs << (shift - 1);
+            res += 1 << (shift - 1);
+        }
+        return res * sign;
+    }
+};
