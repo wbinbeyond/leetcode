bb454a26d1c2928783885ee156fbbc5772b0fa96
wbinbeyond <wbin.beyond@gmail.com>
2018-12-18 20:19:43 -0500

    Create Course Schedule.cpp

---
+++ Course Schedule.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        for (const auto& p : prerequisites)
+        {
+            graph[p.first].push_back(p.second);
+        }
+        
+        vector<int> visit(numCourses, 0);
+        for (int i = 0; i < numCourses; ++i)
+        {
+            if (isCycle(i, visit, graph)) return false;
+        }
+        
+        return true;
+    }
+    
+    bool isCycle(int root, vector<int>& visit, const vector<vector<int>>& graph)
+    {
+        if (visit[root] == 2) return false;  //visited
+        if (visit[root] == 1) return true;   //visiting
+        visit[root] = 1;
+        for (const auto& n : graph[root])
+        {
+            if (isCycle(n, visit, graph)) return true;
+        }
+        visit[root] = 2;
+        return false;
+    }
+};
+
+
+
+// below is wrong, need to see why
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        for (const auto& p : prerequisites)
+        {
+            graph[p.first].push_back(p.second);
+        }
+        
+        vector<int> visit(numCourses, 0);
+        for (int i = 0; i < numCourses; ++i)
+        {
+            if (isCycle(i, visit, graph)) return false;
+        }
+        
+        return true;
+    }
+    
+    bool isCycle(int root, vector<int>& visit, const vector<vector<int>>& graph)
+    {
+        if (visit[root] == 1) return true;   //visited
+        visit[root] = 1;
+        for (const auto& n : graph[root])
+        {
+            if (visit[root] == 1)
+            {
+                return true;
+            }
+            else
+            {
+                if (isCycle(n, visit, graph)) return true;
+            }
+        }
+        visit[root] = 0;
+        return false;
+    }
+};
