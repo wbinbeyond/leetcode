1d60d27fb5047f01867d5b27ad85dd7fd7069483
wbinbeyond <wbin.beyond@gmail.com>
2019-01-01 22:08:28 -0500

    Update Minimum Window Substring.cpp

--- Minimum Window Substring.cpp
+++ Minimum Window Substring.cpp
@@ -36,3 +36,43 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> T;
+        for (const auto& c : t) {
+            T[c]++;
+        }
+        
+        int start = 0,
+            end = 0,
+            len = INT_MAX,
+            counter = T.size();  //unique character in t
+        string res = "";
+        
+        while (end < s.size()) {
+            char endChar = s[end];
+            if (T.find(endChar) != T.end()) {
+                T[endChar]--;
+                if (T[endChar] == 0) counter--;
+            }
+            while (counter == 0) {
+                //valid substring
+                if (end - start + 1 < len) {
+                    len = end - start + 1;
+                    res = s.substr(start, len);
+                }
+                char startChar = s[start];
+                if (T.find(startChar) != T.end()) {
+                    T[startChar]++;
+                    if (T[startChar] > 0) counter++;
+                }
+                start++;
+            }
+            end++;  //move end++ down here, easier than the above one to understand
+        }
+        return res;
+    }
+};
