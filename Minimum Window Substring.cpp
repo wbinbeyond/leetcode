db63d7c7d660f2c1d30b57305bd98eb5c1c29863
wbinbeyond <wbin.beyond@gmail.com>
2019-01-01 22:04:09 -0500

    Create Minimum Window Substring.cpp

---
+++ Minimum Window Substring.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char, int> T;
+        for (const auto& c : t) {
+            T[c]++;
+        }
+        
+        int start = 0,
+            end = 0,
+            len = INT_MAX,
+            counter = T.size();  //unique character in t
+        string res = "";
+        
+        while (end < s.size()) {
+            char endChar = s[end];
+            if (T.find(endChar) != T.end()) {
+                T[endChar]--;
+                if (T[endChar] == 0) counter--;
+            }
+            end++;
+            while (counter == 0) {
+                //valid substring
+                if (end - start < len) {
+                    len = end - start;
+                    res = s.substr(start, len);
+                }
+                char startChar = s[start];
+                if (T.find(startChar) != T.end()) {
+                    T[startChar]++;
+                    if (T[startChar] > 0) counter++;
+                }
+                start++;
+            }
+        }
+        return res;
+    }
+};
