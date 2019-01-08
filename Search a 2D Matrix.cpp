3d85adfadef766da99151d362f30e4062cd2ac44
wbinbeyond <wbin.beyond@gmail.com>
2019-01-07 21:56:54 -0500

    Create Search a 2D Matrix.cpp

---
+++ Search a 2D Matrix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) return false;
+        int m = matrix.size(),
+            n = matrix[0].size();
+        int left = 0;
+        int right = m * n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midNum = matrix[mid / n][mid % n];
+            if (midNum == target) {
+                return true;
+            }
+            else if (midNum < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
