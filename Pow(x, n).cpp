dbb1e0b1d56828060d553343b110921fd311777b
wbinbeyond <wbin.beyond@gmail.com>
2019-01-10 19:23:38 -0500

    Create Pow(x, n).cpp

---
+++ Pow(x, n).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n < 0) return 1 / power(x, -n);
+        return power(x, n);
+    }
+private:
+    double power(double x, int n) {
+        if (n == 0) return 1;
+        double half = power(x, n/2);
+        if (n % 2 == 0) {
+            return half * half;
+        }
+        else {
+            return x * half * half;
+        }
+    }
+};
