f312f98b7d57495b64092c84c6c123af56c3adb9
wbinbeyond <wbin.beyond@gmail.com>
2019-02-13 21:26:17 -0500

    Update Implement Trie (Prefix Tree).cpp

--- Implement Trie (Prefix Tree).cpp
+++ Implement Trie (Prefix Tree).cpp
@@ -1,5 +1,62 @@
 class Trie {
 public:
+    struct TrieNode {
+        bool isWord;
+        TrieNode* children[26];
+    };
+    
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        TrieNode* p = root;
+        for (const auto& c : word) {
+            int i = c - 'a';
+            if (!p->children[i]) {
+                p->children[i] = new TrieNode();
+            }
+            p = p->children[i];
+        }
+        p->isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        TrieNode* res = find(word);
+        return res && res->isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        return find(prefix) != nullptr;
+    }
+private:
+    TrieNode* root;
+    TrieNode* find(string s) {
+        TrieNode* p = root;
+        for (const auto& c : s) {
+            int i = c - 'a';
+            if (!p->children[i]) return nullptr;
+            p = p->children[i];
+        }
+        return p;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * bool param_2 = obj.search(word);
+ * bool param_3 = obj.startsWith(prefix);
+ */
+
+
+class Trie {
+public:
     /** Initialize your data structure here. */
     Trie(): root(new TrieNode()) {
         
