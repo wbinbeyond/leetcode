98bfe6a7f2b2b18b40adc2544a41aeb3d55f4167
wbinbeyond <wbin.beyond@gmail.com>
2019-02-26 21:21:16 -0500

    Update Coin Change.cpp

--- Coin Change.cpp
+++ Coin Change.cpp
@@ -20,3 +20,19 @@ public:
         return memo[amount];
     }
 };
+
+
+class Solution {
+public: 
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> memo(amount + 1, amount + 1);
+        memo[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (int j = 0; j < coins.size(); ++j) {
+                if (coins[j] <= i) 
+                    memo[i] = min(memo[i], memo[i-coins[j]] + 1);
+            }
+        }
+        return memo[amount] > amount ? -1 : memo[amount];
+    }
+};
